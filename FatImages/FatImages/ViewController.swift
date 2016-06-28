d6137f15325667d5b3aea8798bdc532abda8413b
Joshua Kelley <jdkelley@samaritan.org>
2016-06-28 15:14:18 -0400

    Wired up initial project state

--- FatImages/FatImages/ViewController.swift
+++ FatImages/FatImages/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
+enum BigImage: String {
+    case whale = "https://lh3.googleusercontent.com/16zRJrj3ae3G4kCDO9CeTHj_dyhCvQsUDU0VF0nZqHPGueg9A9ykdXTc6ds0TkgoE1eaNW-SLKlVrwDDZPE=s0#w=4800&h=3567"
+    case shark = "https://lh3.googleusercontent.com/BCoVLCGTcWErtKbD9Nx7vNKlQ0R3RDsBpOa8iA70mGW2XcC76jKS09pDX_Rad6rjyXQCxngEYi3Sy3uJgd99=s0#w=4713&h=3846"
+    case seaLion = "https://lh3.googleusercontent.com/ibcT9pm_NEdh9jDiKnq0NGuV2yrl5UkVxu-7LbhMjnzhD84mC6hfaNlb-Ht0phXKH4TtLxi12zheyNEezA=s0#w=4626&h=3701"
+}
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBAction func synchronousDownload(sender: UIBarButtonItem) {
+        // <#Code#>
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func simpleAsyncronousDownload(sender: UIBarButtonItem) {
+        // <#Code#>
     }
-
+    
+    @IBAction func asynchronousDownload(sender: UIBarButtonItem) {
+        // <#Code#>
+    }
+    
+    @IBAction func setTransparencyOfImage(sender: UISlider) {
+        photoView.alpha = CGFloat(sender.value)
+    }
 
 }
 
