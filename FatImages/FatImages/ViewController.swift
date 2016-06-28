4c12343443533f00a09e617bf1e8b50ab9042aac
Joshua Kelley <jdkelley@samaritan.org>
2016-06-28 14:59:42 -0400

    Add FatImages initial project.

---
+++ FatImages/FatImages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FatImages
+//
+//  Created by Joshua Kelley on 6/28/16.
+//  Copyright © 2016 Joshua Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
