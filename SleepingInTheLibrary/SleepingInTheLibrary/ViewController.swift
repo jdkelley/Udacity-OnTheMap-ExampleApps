2250d3d9d98f28b2fce0f734e2a789206a79c59f
Joshua Kelley <jdkelley@samaritan.org>
2016-06-30 11:57:41 -0400

    Project: SleepingInTheLibrary. Add initial project.

---
+++ SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepingInTheLibrary
+//
+//  Created by Joshua Kelley on 6/30/16.
+//  Copyright © 2016 Joshua Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
